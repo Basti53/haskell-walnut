4c3c35d21b95184c049922e27b36b9e12800be47
Basti <sebastiankarlsson29@gmail.com>
2022-11-16 11:53:39 +0200

    Some progress.

--- Walnut.hs
+++ Walnut.hs
@@ -2,7 +2,16 @@
 {- stack script --resolver lts-17.9
   --package process
 -}
+import System.IO
 import System.Process
 
+javaPath = "C:\\Program Files\\Common Files\\Oracle\\Java\\javapath\\java.exe"
+walnutPath = "C:\\Users\\Basti\\Desktop\\Walnut\\bin\\"
+
 main :: IO ()
-main = callProcess "java" ["--version"]
+-- main = readProcess javaPath ["-classpath", walnutPath, "Main.Prover"] "eval this \"Ex x=x\":" >>= putStrLn
+main = do
+    (Just hin, Just hout, _, ph) <- createProcess (proc "java" ["Main.Prover"]){cwd=Just walnutPath, std_out=CreatePipe, std_in=CreatePipe}
+    hPutStrLn hin "eval this \"Ex x=0\":"
+    str <- hGetContents hout
+    putStrLn str
